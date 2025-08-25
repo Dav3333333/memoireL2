7a211179b1228f94c349ef769918214d6793e336
Dav3333333 <davidlusenge7@gmail.com>
2025-08-25 10:30:34 -0700

    adding the menu btn to buttons

--- src/js/controller/transporter/transporter.js
+++ src/js/controller/transporter/transporter.js
@@ -14,17 +14,34 @@ class Transporters {
                     <div class="cooperative-info">
                         <h3 class="cooperative-info__title">TMK</h3>
                     </div>
+                    <div class="cooperative-stock">
+                        <span class="text-small text-thin">Contacts: +243 81 83 01 (tmk@tmkcongo.com)</span>
+                    </div>
                 </div>
-
                 <div class="cooperative-card">
                     <div class="cooperative-info">
-                        <h3 class="cooperative-info__title">TMK</h3>
+                        <h3 class="cooperative-info__title">BOLLORE TRANSPORT ET LOGISTIQUE</h3>
+                    </div>
+                    <div class="cooperative-stock">
+                        <span class="text-small text-thin">-</span>
                     </div>
                 </div>
 
                 <div class="cooperative-card">
                     <div class="cooperative-info">
-                        <h3 class="cooperative-info__title">TMK</h3>
+                        <h3 class="cooperative-info__title">Bleu Blan Logistique</h3>
+                    </div>
+                    <div class="cooperative-stock">
+                        <span class="text-small text-thin">+243 976 767 652 (etsbblm@bleublanclogistiquerdc.org)</span>
+                    </div>
+                </div>
+
+                <div class="cooperative-card">
+                    <div class="cooperative-info">
+                        <h3 class="cooperative-info__title">GTM</h3>
+                    </div>
+                    <div class="cooperative-stock">
+                        <span class="text-small text-thin">+243 991 002 538 / +243 828 024 167 (secretariatgtm@gtmdrc.com) </span>
                     </div>
                 </div>
             </div>
